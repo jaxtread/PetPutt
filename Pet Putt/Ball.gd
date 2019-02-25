69b938794dcb923b434d6acabc9f528dcdf42eb4
jaxtread <47083656+jaxtread@users.noreply.github.com>
2019-02-25 17:10:04 -0500

    Add files via upload

---
+++ Pet Putt/Ball.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.bounce = 0.8
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("launch_ball"):
+		
+		var differenceVector = get_global_mouse_position() - self.position
+		var directionVector = differenceVector.normalized()
+		var speed = 400
+		var launchVector = directionVector * speed
+	
+	pass
